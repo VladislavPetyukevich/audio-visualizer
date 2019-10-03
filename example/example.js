2578538135034a03cf42247e929527487f5bd6d6
VladislavPetyukevich <pet.vladislav@gmail.com>
2019-10-03 23:02:58 +0300

    Render time formating

--- example/example.js
+++ example/example.js
@@ -1,9 +1,21 @@
-const TIME_LABEL = 'rendered in';
 const config = require('./config');
 const renderAudioVisualizer = require('../src');
-console.time(TIME_LABEL);
+const startTime = new Date();
+
+const millisecondsToTime = (milliseconds) => {
+  const seconds = Math.trunc((milliseconds / 1000) % 60);
+  const minutes = Math.trunc((milliseconds / (1000 * 60)) % 60);
+  const hours = Math.trunc((milliseconds / (1000 * 60 * 60)) % 24);
+
+  const hoursFormated = (hours < 10) ? `0${hours}` : hours;
+  const minutesFormated = (minutes < 10) ? `0${minutes}` : minutes;
+  const secondsFormated = (seconds < 10) ? `0${seconds}` : seconds;
+
+  return `${hoursFormated}:${minutesFormated}:${secondsFormated}`;
+};
+
 renderAudioVisualizer(config)
   .then((exitCode) => {
     console.log(`exited with code: ${exitCode}`);
-    console.timeEnd(TIME_LABEL);
+    console.log(`rendered in: `, millisecondsToTime((new Date()) - startTime));
   });
