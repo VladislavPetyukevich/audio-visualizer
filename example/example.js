b1286c1ad5e4d01fc29f7a5178d6d0c7402b0919
VladislavPetyukevich <pet.vladislav@gmail.com>
2019-10-03 20:17:13 +0300

    Add render time info in example

--- example/example.js
+++ example/example.js
@@ -1,6 +1,9 @@
+const TIME_LABEL = 'rendered in';
 const config = require('./config');
 const renderAudioVisualizer = require('../src');
+console.time(TIME_LABEL);
 renderAudioVisualizer(config)
   .then((exitCode) => {
     console.log(`exited with code: ${exitCode}`);
+    console.timeEnd(TIME_LABEL);
   });
